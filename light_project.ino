577a3da6802ca421bb059a3880f24c9e1a099bed
Kyle Pedersen <kylepedersen@berkeley.edu>
2019-07-08 22:52:07 -0700

    brightness working only when turned on and off again

--- light_project.ino
+++ light_project.ino
@@ -2,14 +2,18 @@ int trigPin=2;
 int echoPin=3;
 float signal; //time for pint to hit target and return 
 
+int potPin=A0; //assign potentiometer pin to A0
+int readValue; //declare potentiometer readValue variable
+float brightness;
+
 int redPin=10;  //set red LED pin to 11
 int greenPin=9; //set green LED pin to 10
 int bluePin=5; //set blue LED pin to 6
-int brightness=100; //Set brightness to 100
 boolean onStatus = false;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(potPin, INPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(redPin, OUTPUT);  
@@ -18,17 +22,21 @@ void setup() {
 }
 
 void loop() {
-    // potentiometer
+    readValue = analogRead(potPin);
+    brightness = (255./1000.) * readValue;
+    Serial.println(brightness);
+    
+    // readValue = analogRead(potPin);
     digitalWrite(trigPin, LOW);
     delay(2);
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
 
-    // getting signal from potentiometer
+    // getting signal from ultrasonic
     signal = pulseIn(echoPin, HIGH); //measure ping time in microseconds
     int newSignal = (int) signal;
-    Serial.println(newSignal);
+//    Serial.println(newSignal);
 
     
     //decide whether or not to turn on light
@@ -38,14 +46,12 @@ void loop() {
       analogWrite(bluePin, 0); //write 100 (brightness) to blue pin
       onStatus = false;   
     }
-    else if (newSignal < 500) {
-      Serial.println("HI");    
-      onStatus = true;
+    else if (newSignal < 500) { 
       analogWrite(redPin, brightness); //turn off red pin
       analogWrite(greenPin, 0); //turn off green pin
       analogWrite(bluePin, 0); //write 100 (brightness) to blue pin
+      onStatus = true;
     } 
-   
-
+  
   delay(400);
 }
