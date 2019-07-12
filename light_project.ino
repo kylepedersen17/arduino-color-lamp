9503175754425374cc18ba37354df71e4d2562c1
Kyle Pedersen <kylepedersen@berkeley.edu>
2019-07-11 22:46:34 -0700

    working code

--- light_project.ino
+++ light_project.ino
@@ -1,14 +1,26 @@
+//ultrasonic sensor variables
 int trigPin=2;
 int echoPin=3;
+int buttonPin=12;
 float signal; //time for pint to hit target and return 
 
-int potPin=A0; //assign potentiometer pin to A0
-int readValue; //declare potentiometer readValue variable
-float brightness;
+// potentiometer variables
+int potPin=A0; 
+int readValue;
 
-int redPin=9;  //set red LED pin to 11
-int greenPin=11; //set green LED pin to 10
-int bluePin=10; //set blue LED pin to 6
+// RBG color variables
+int i = 0; // color index counter
+int reds[]   = {255, 0, 0, 255,};
+int greens[] = {0, 0, 255, 0, 255};
+int blues[]  = {0, 255, 255, 255}; //red, blue, cyan, magenta
+float brightnessRed = reds[i];
+float brightnessBlue = greens[i];
+float brightnessGreen = blues[i];
+int redPin=9; 
+int greenPin=11; 
+int bluePin=10; 
+
+//light starts at off position
 boolean onStatus = false;
 
 void setup() {
@@ -22,40 +34,60 @@ void setup() {
 }
 
 void loop() {
+    //sets brightness variables based on potentiometer position
     readValue = analogRead(potPin);
-    brightness = (255./1000.) * readValue;
-    Serial.println(brightness);
+    brightnessRed = (255./1000.) * readValue;
+    brightnessGreen = (255./1000.) * readValue;
+    brightnessBlue = (255./1000.) * readValue;
     
-    // readValue = analogRead(potPin);
+    
+    //sends out ultrasonic sensor ping signal
     digitalWrite(trigPin, LOW);
     delay(2);
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
-
+    
     // getting signal from ultrasonic
     signal = pulseIn(echoPin, HIGH); //measure ping time in microseconds
     int newSignal = (int) signal;
-//    Serial.println(newSignal);
-
     
-    //decide whether or not to turn on light
-    if (newSignal < 500 && onStatus) { 
-      analogWrite(redPin, 0); //turn off red pin
-      analogWrite(greenPin, 0); //turn off green pin
-      analogWrite(bluePin, 0); //write 100 (brightness) to blue pin
-      onStatus = false;   
-    }
-    else if (newSignal < 500) { 
-      analogWrite(redPin, brightness); //turn off red pin
-      analogWrite(greenPin, 0); //turn off green pin
-      analogWrite(bluePin, 0); //write 100 (brightness) to blue pin
-      onStatus = true;
-    } 
-
-    if (onStatus) { //use serial disrupt in future for smoother function
-      analogWrite(redPin, brightness);
-    }
+   
+  //decide whether or not to turn on light
+  if (newSignal < 500 && onStatus) { 
+    //turns off led
+    analogWrite(redPin, 0);
+    analogWrite(greenPin, 0); 
+    analogWrite(bluePin, 0); 
+    onStatus = false;   
+  } else if (newSignal < 500) { 
+    //turns on led 
+    analogWrite(redPin, brightnessRed); 
+    analogWrite(greenPin, brightnessGreen); 
+    analogWrite(bluePin, brightnessBlue); 
+    onStatus = true;
+  } 
   
-  delay(400);
+  
+  
+  //changes color if button is pressed
+  if (digitalRead(buttonPin) == 1) {
+    brightnessRed = reds[i + 1];
+    brightnessGreen = greens[i + 1];
+    brightnessBlue = blues[i + 1];
+    i++;
+    if (i > sizeof(reds)) {
+      i = 0;
+    }
+  }
+  
+  
+  //sets brightness according to potentiometer changes
+      if (onStatus) { 
+        analogWrite(redPin, brightnessRed);
+        analogWrite(greenPin, brightnessGreen);
+        analogWrite(bluePin, brightnessBlue);
+      }
+    
+    delay(400);
 }
